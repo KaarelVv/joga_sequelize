ae732ac4a918efb4a222ce39c68aaee11e02036f
Kaarel <viilvere.k@gmail.com>
2026-02-20 13:05:06 +0200

    Create article table and added table data

---
+++ seeders/20260220105830-demo-articles.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Articles', [
+      {
+        name: 'First Article',
+        slug: 'first-article',
+        image: 'https://example.com/image1.jpg',
+        body: 'This is the body of the first article.',
+        published: '2026-02-20',
+       
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Second Article',
+        slug: 'second-article',
+        image: 'https://example.com/image2.jpg',
+        body: 'This is the body of the second article.',
+        published: '2026-02-20',
+       
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Articles', null, {});
+  }
+};
